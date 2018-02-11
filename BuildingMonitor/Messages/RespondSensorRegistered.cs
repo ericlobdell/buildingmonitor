0ddefff9097400fdb6e4ab328e43303622ff87fa
Eric Lobdell <eric@ericlobdell.me>
2018-02-11 17:30:50 -0500

    TemperatureSensor and tests

---
+++ BuildingMonitor/Messages/RespondSensorRegistered.cs
@@ -0,0 +1,21 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingMonitor.Messages
+{
+  public sealed class RespondSensorRegistered
+  {
+    public long RequestId { get; }
+    public IActorRef SensorReference { get; }
+
+    public RespondSensorRegistered(long requestId, IActorRef sensorReference)
+    {
+      RequestId = requestId;
+      SensorReference = sensorReference;
+    }
+  }
+}
